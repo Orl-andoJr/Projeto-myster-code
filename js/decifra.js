f8cc089569345f82dc12ea99e49b597bf2201127
Orl-andoJr <112992075+Orl-andoJr@users.noreply.github.com>
2022-11-03 20:12:35 -0300

    subindo projeto myster code

---
+++ js/decifra.js
@@ -0,0 +1,68 @@
+/* Mostrar a parte de incremento da Cifra */
+let escolhas = document.querySelector("select");
+
+escolhas.addEventListener("change", function (evento) {
+  evento.preventDefault();
+
+  let incremento = document.getElementById("valorIncremento");
+
+  if (evento.target.value == "cCesar") {
+    incremento.style = "display: flex";
+  } else {
+    incremento.style = "display: none";
+  }
+});
+
+
+
+/* CRIPTOGRAFIAS */
+
+let formulario = document.forms.formulario;
+
+formulario.addEventListener("submit", function (evento) {
+  evento.preventDefault();
+
+  let texto = formulario.texto.value;
+  let escolha = formulario.escolherCod.value;
+  let botoes = formulario.codific.value;
+  let numeroIncremento = formulario.numeroIncrementos.value;
+  let mensagemFinal = "";
+
+  if (escolha == "cCesar") {
+    mensagemFinal = cesar(botoes, texto, numeroIncremento);
+  } else {
+    mensagemFinal = base64(botoes, texto);
+  }
+
+  let resultadoTexto = document.getElementById("saidaTexto");
+  resultadoTexto.innerHTML = `${mensagemFinal}`;
+});
+
+/* Cifra */
+function cesar(codific, texto, numeroIncremento) {
+  numeroIncremento = Number(numeroIncremento);
+
+  let mensagemFinal = "";
+
+  for (let i = 0; i < texto.length; i++) {
+    let letra = texto[i];
+    let codigo = letra.charCodeAt();
+
+    if (codific == "codificar") {
+      codigo += numeroIncremento;
+    } else {
+      codigo -= numeroIncremento;
+    }
+    mensagemFinal += String.fromCharCode(codigo);
+  }
+  return mensagemFinal;
+}
+
+/* Base */
+function base64(codific, texto) {
+  if (codific == "codificar") {
+    return btoa(texto);
+  } else {
+    return atob(texto);
+  }
+}
